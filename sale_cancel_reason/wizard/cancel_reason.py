e3e12506cc7b5d506a87bfc101649432bcc595ae
yiğit budak <yibudak@gmail.com>
2022-07-29 10:02:45 +0300

    [12.0][FIX] server side group check

--- sale_cancel_reason/wizard/cancel_reason.py
+++ sale_cancel_reason/wizard/cancel_reason.py
@@ -1,6 +1,7 @@
 # © 2013 Guewen Baconnier, Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
 
 
 class SaleOrderCancel(models.TransientModel):
@@ -23,6 +24,10 @@ class SaleOrderCancel(models.TransientModel):
             return act_close
         assert len(sale_ids) == 1, "Only 1 sale ID expected"
         sale = self.env['sale.order'].browse(sale_ids)
+
+        if sale.state == 'sale' and not self.env.user.has_group('__export__.res_groups_156_a480bcf3'):
+            raise UserError(_('You are not allowed to cancel this sale order.'))
+
         sale.cancel_reason_id = self.reason_id.id
         sale.action_cancel()
         return act_close
